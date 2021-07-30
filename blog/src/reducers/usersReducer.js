eaa2eac68dd151781ae12e8960276f509eafcb07
bgora <bartlomiej.gora@infobip.com>
2021-07-30 15:59:02 +0200

    Redux part finished

--- blog/src/reducers/usersReducer.js
+++ blog/src/reducers/usersReducer.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = [], action) => {
   switch (action.type) {
-    case "FETCH_USER":
-      return [...state, action.paylod]
+    case 'FETCH_USER':
+      return [...state, action.payload]
     default:
       return state;
   }
