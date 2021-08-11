9465a628f38329a9d73de71eda6248df637a4f36
bgora <bartlomiej.gora@infobip.com>
2021-08-11 15:54:07 +0200

    router lesson

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
-const PageOne = () => {
-    return <div>Page One</div>;
-};
 
-const PageTwo = () => {
-    return <div>Page Two</div>;
-};
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                </div>
             </BrowserRouter>
         </div>
     );
