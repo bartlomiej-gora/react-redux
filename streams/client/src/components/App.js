a09a795b79561e61bb7498ad172c8c24a3e214df
bartlomiej-gora <bartlomiej.gora@gmail.com>
2021-09-20 15:58:05 +0200

    Redux-formsgit add .

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -7,23 +7,21 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 
-
-
 const App = () => {
-    return (
-        <div className="ui container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </div>
-            </BrowserRouter>
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-    );
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
